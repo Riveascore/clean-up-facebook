9d7a8691c06100bddc71f0a4d66f84be8f55c7d8
Riveascore <ChrisAga.dev@gmail.com>
2016-05-07 00:23:58 -0500

    Search posts

--- main_file.rb
+++ main_file.rb
@@ -6,7 +6,19 @@ require 'pry-stack_explorer'
 oauth_access_token = ENV['FACEBOOK_GRAPH_ACCESS_TOKKEN']
 
 @graph = Koala::Facebook::API.new(oauth_access_token)
+#
+# profile = @graph.get_object("me")
+# friends = @graph.get_connections("me", "friends")
+# binding.pry_remote
 
-profile = @graph.get_object("me")
-friends = @graph.get_connections("me", "friends")
-binding.pry_remote
+field_values = ['message', 'id', 'from', 'type', 'picture', 'link', 'created_time', 'updated_time']
+
+max_items = 5000
+
+hash_values = {
+  limit: max_items,
+  fields: field_values
+}
+
+client = Koala::Facebook::API.new(oauth_access_token)
+stuff = client.get_connection('me', 'posts', hash_values)
